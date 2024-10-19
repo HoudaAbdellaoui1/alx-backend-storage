363254d6139eda7ea3e668108b946eced4500db4
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2024-10-19 02:43:39 +0100

    calculate longevity

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,5 @@
 -- SQL Query to select band name and calculate lifespan
-SELECT band_name, IFNULL(split, 2022) - formed AS lifespan
+SELECT band_name, IFNULL(2022 - split, 2022 - formed) AS lifespan
 FROM metal_bands
 WHERE style like "%Glam rock%"
 ORDER BY lifespan DESC
