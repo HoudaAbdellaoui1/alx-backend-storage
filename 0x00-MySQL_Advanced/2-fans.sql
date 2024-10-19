a59047e11ff16676ff5e7fbd650839870729f786
HoudaAbdellaoui1 <78303915+HoudaAbdellaoui1@users.noreply.github.com>
2024-10-19 02:18:13 +0100

    Rank country origins of bands

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,5 @@
 -- SQL Query to retrieve the origin and number of fans, ordered by fans descending
 SELECT origin, fans AS nb_fans 
-FROM metal_bands 
+FROM metal_bands
+GROUP BY origin
 ORDER BY fans DESC;
